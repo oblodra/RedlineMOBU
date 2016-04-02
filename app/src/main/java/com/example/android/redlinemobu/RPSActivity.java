725529ce7519de490dc48065e55fe2aca1a05277
oblodra <btgpetersson@gmail.com>
2016-04-02 19:43:23 +0200

    Added RPS layout basics.

---
+++ app/src/main/java/com/example/android/redlinemobu/RPSActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.redlinemobu;
+
+import android.app.Activity;
+import android.view.View;
+
+/**
+ * Created by Bengt on 2016-04-02.
+ */
+public class RPSActivity extends Activity{
+
+
+    public void onChooseRock(View view) {
+
+    }
+
+    public void onChoosePaper(View view) {
+
+    }
+
+    public void onChooseScissor(View view) {
+
+    }
+
+    public void onRPSQuitClick(View view) {
+
+    }
+}
